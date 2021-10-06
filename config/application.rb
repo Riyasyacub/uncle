f664ca71e9d18492b3b3ed25e78db9330b9b7e86
Riyasyacub <59021328+Riyasyacub@users.noreply.github.com>
2021-10-06 07:32:24 +0000

    date Time

--- config/application.rb
+++ config/application.rb
@@ -37,12 +37,13 @@ module Rails2
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Chennai"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Allow framing to see output in Repl.it
     config.action_dispatch.default_headers = {
       'X-Frame-Options' => ''
     }
+    # config.time_zone = "Chennai"
   end
 end
