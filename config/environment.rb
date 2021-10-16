acef14c9247657289c48ec0161ef48e05ed4cebf
Riyasyacub <59021328+Riyasyacub@users.noreply.github.com>
2021-10-16 16:35:21 +0000

    mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# Add sendgrid support
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :user_name => "postmaster@sandboxb2dc7280df444242beebedce22574e26.mailgun.org",
+    :password => "6d209d8a7bd22e813d118b2ab0125387-2ac825a1-2983b8ec",
+    :domain => "www.alnafistravels.in",
+    :address => "smtp.mailgun.org",
+    :port => 587, # sendgrid_port
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
